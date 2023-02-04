ef46921eafaa88ae8075c535ddb700184c9dc47f
Christina V <christina.vuong@mail.mcgill.ca>
2023-02-04 00:53:55 -0500

    templateURL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: '/StockChart/src/app/app.component.html',
-  // templateUrl: '../../../StockChart/src/app/app.component.html',
+  templateUrl : './src/app/app.component.html',
+  //templateUrl: '../../../StockChart/src/app/app.component.html',
   styleUrls: ['../../../StockChart/src/app/app.component.css']
 })
 export class AppComponent {
